e17183067d448c486dbf68d177acca3e64a1e3b6
Danny Fuhriman <danny.fuhriman@Okta.com>
2024-05-13 15:41:04 -0700

    improve asset bundling

--- vite.config.ts
+++ vite.config.ts
@@ -6,22 +6,38 @@ import type { UserConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-	const defaultConfig: UserConfig = {
+	let defaultConfig: UserConfig = {
 		plugins: [react(), tsConfigPaths()],
 		build: {
 			rollupOptions: {
 				output: {
 					entryFileNames: `bundle.js`,
 					chunkFileNames: `bundle.js`,
-					assetFileNames: `[name].[ext]`,
+					assetFileNames: (assetInfo) => {
+						const fontRegex = /\.(woff2?|eot|ttf|otf)(\?.*)?$/;
+
+						if (assetInfo?.name && fontRegex.test(assetInfo.name)) {
+							return `fonts/${assetInfo.name}`;
+						}
+						return `[name].[ext]`;
+					},
 				},
 			},
 		},
 	};
 
 	if (mode !== 'production') {
-		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-		defaultConfig.build!.minify = false;
+		defaultConfig = {
+			...defaultConfig,
+			build: {
+				...defaultConfig?.build,
+				minify: false,
+			},
+			server: {
+				...defaultConfig?.server,
+				port: 3000,
+			},
+		};
 	}
 
 	return defaultConfig;
